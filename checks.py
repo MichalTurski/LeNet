bc99579aa147cf2062256bc256080effef57805a
MichalTurski <turski.michal.antoni@gmail.com>
2019-04-11 22:26:47 +0200

    Added ROC curves ad pickling best network.

--- checks.py
+++ checks.py
@@ -1,5 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import roc_curve, auc
 
 
 def test_loss(test_loader, net, device, loss_function):
@@ -43,3 +45,29 @@ def plot(train_loss, test_loss, accuracy):
     axes = plt.gca()
     axes.set_ylim(bottom=0)
     plt.show()
+
+
+def roc_curves(net, test_loader, classes, device):
+    with torch.no_grad():
+        outputs = []
+        labels_vec = []
+        for (inputs, labels) in test_loader:
+            inputs = inputs.to(device)
+            outputs_cpu = net(inputs).cpu()
+            for output in outputs_cpu:
+                outputs.append(output.detach().numpy())
+            for label in labels:
+                labels_vec.append(label.detach().numpy())
+    outputs_array = np.array(outputs)
+    labels_array = np.array(labels_vec)
+    for i in range(10):
+        digit_pred = outputs_array[:, i]
+        y_expected = labels_array == i
+        fpr, tpr, thresholds = roc_curve(y_expected, digit_pred)
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, lw=2, alpha=0.9, color='r', label='ROC curve')
+        plt.plot([0, 1], [0, 1], linestyle='--', lw=1, color='g', label='Random classifier', alpha=0.4)
+        plt.title(f"ROC curve for class {classes[i]}, AUC = {roc_auc}")
+        plt.legend(loc='lower right')
+        plt.show()
+
--- run.py
+++ run.py
@@ -5,27 +5,29 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import copy
+import pickle
 
 ##########
 # Params #
 ##########
-epochs = 150
+epochs = 15
 batch_size = 32
 num_workers = 4
-learning_rate = 0.001
+learning_rate = 0.005
 momentum = 0.9
 verbose = True
 check_period = 750
-loss_rise_threshold = 4
-net = network.LeNet()
+loss_rise_threshold = 5
+# net = network.LeNet()
+net = pickle.load(open( "best_net.pickle", "rb"))
 # net = network.ResNet()
+# net = network.VGG()
 # net = models.resnet18(pretrained=True)
 # net.fc = nn.Linear(512, 10)
 # net = models.AlexNet()
 # net.classifier[6] = nn.Linear(4096, 10)
 
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-device = "cuda:0"
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 train_loader, test_loader, classes = data_loading.prepare_data(batch_size, num_workers)
 net.to(device)
 
@@ -44,6 +46,7 @@ if verbose:
           f'test loss = {test_loss:.3f}, accuracy = {accuracy * 100:.2f}%')
 
 prev_loss = float("inf")
+lowest_loss = float("inf")
 loss_rise_count = 0
 best_net = copy.deepcopy(net)
 
@@ -74,8 +77,14 @@ for epoch in range(epochs):
         if loss_rise_count >= loss_rise_threshold:
             break
     else:
-        best_net = copy.deepcopy(net)
         loss_rise_count = 0
+
+    if test_loss < lowest_loss:
+        best_net = copy.deepcopy(net)
+        lowest_loss = test_loss
+
     prev_loss = test_loss
 checks.plot(train_loss_list, test_loss_list, accuracy_list)
+# checks.roc_curves(best_net, test_loader, classes, device)
+pickle.dump(best_net, open("best_net.pickle", "wb"))
 
